686129a0327b31ea068b9bcd7a6d9847ee4bbef5
arehovski <sergei_arehovski@mail.ru>
2020-09-01 17:40:23 +0300

    Created detail movie reply.

--- database.py
+++ database.py
@@ -6,17 +6,16 @@ class DBManager:
     conn: asyncpg.Connection = conn
     GENRES = "SELECT genre from movies_genre"
     MOVIES_FROM_GENRE = "SELECT * FROM movies_movie JOIN movies_movie_genre mmg on movies_movie.id = mmg.movie_id " \
-                       "JOIN movies_genre mg on mg.id = mmg.genre_id where genre = $1"
+                        "JOIN movies_genre mg on mg.id = mmg.genre_id where genre = $1"
     MOVIE_FROM_PK = ["SELECT title, year, duration_min, description, rating_kp, link_kp FROM movies_movie where id = $1",
-                     "SELECT mg.genre FROM movies_genre JOIN movies_movie_genre mmg on movies_genre.id = mmg.genre_id "
-                     "JOIN movies_genre mg on mg.id = mmg.genre_id WHERE movie_id = $1",
-                     "SELECT mc.country FROM movies_country JOIN movies_movie_country mmc on "
-                     "movies_country.id = mmc.country_id JOIN movies_country mc on "
-                     "mc.id = mmc.country_id where movie_id = $1",
-                     "SELECT first_name, last_name FROM movies_director JOIN movies_movie mm "
-                     "on movies_director.id = mm.director_id where mm.id = $1",
-                     "SELECT ma.first_name, ma.last_name FROM movies_actor JOIN movies_movie_actors mma on "
-                     "movies_actor.id = mma.actor_id JOIN movies_actor ma on ma.id = mma.actor_id WHERE movie_id = $1"]
+                     "SELECT mg.genre FROM movies_genre mg JOIN movies_movie_genre mmg on mg.id = mmg.genre_id "
+                     "WHERE mmg.movie_id = $1",
+                     "SELECT mc.country FROM movies_country mc JOIN movies_movie_country mmc on mc.id = mmc.country_id "
+                     "where mmc.movie_id = $1",
+                     "SELECT md.first_name, md.last_name FROM movies_director md JOIN movies_movie mm "
+                     "on md.id = mm.director_id where mm.id = $1",
+                     "SELECT ma.first_name, ma.last_name FROM movies_actor ma JOIN movies_movie_actors mma "
+                     "on ma.id = mma.actor_id WHERE mma.movie_id = $1"]
 
     async def get_all_genres(self):
         genres = await self.conn.fetch(self.GENRES)
@@ -26,19 +25,15 @@ class DBManager:
         return await self.conn.fetch(self.MOVIES_FROM_GENRE, genre)
 
     async def get_movie_from_pk(self, pk):
-        movie_data = await self.conn.fetch(self.MOVIE_FROM_PK[0], pk)
-        genres = await self.conn.fetch(self.MOVIE_FROM_PK[1], pk)
-        countries = await self.conn.fetch(self.MOVIE_FROM_PK[2], pk)
-        director = await self.conn.fetch(self.MOVIE_FROM_PK[3], pk)
-        actor = await self.conn.fetch(self.MOVIE_FROM_PK[4], pk)
+        movie_data, genres, countries, director, actors = \
+            [await self.conn.fetch(query, pk) for query in self.MOVIE_FROM_PK]
         return {
             'movie_data': movie_data,
             'genres': genres,
             'countries': countries,
             'director': director,
-            'actors': actor
+            'actors': actors
         }
-
 
 
 db_manager = DBManager()
--- handlers.py
+++ handlers.py
@@ -2,6 +2,7 @@ import random
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, \
     InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.dispatcher.filters import Command, Text
+from aiogram.utils.markdown import text, bold, italic, code, pre
 from main import dp, bot
 from database import db_manager
 
@@ -37,7 +38,26 @@ async def detail_movie_view(message: Message):
     movie_id = message.text[1:]
     movie = await db_manager.get_movie_from_pk(int(movie_id))
     if movie:
-        # TODO здесь будет распаковка и представление данных из фильма
-        pass
+        movie_data = movie.get('movie_data')[0]
+        title = movie_data.get('title')
+        year = str(movie_data.get('year'))
+        duration = str(movie_data.get('duration_min'))
+        description = movie_data.get('description')
+        rating = str(round(movie_data.get('rating_kp'), 1))
+        link_kb = f'http://161.35.201.129/movie/{movie_id}'
+        link_kp = movie_data.get('link_kp')
+        genres = ', '.join([genre.get('genre') for genre in movie.get('genres')])
+        countries = ', '.join([country.get('country') for country in movie.get('countries')])
+        director = movie.get('director')[0]
+        director = ' '.join((director.get('first_name'), director.get('last_name')))
+        actors = ', '.join([' '.join((actor.get('first_name'), actor.get('last_name'))) for actor in movie.get('actors')])
+        msg = f"<b>{title}</b>\n<i>Год:</i> {year}\n<i>Длительность:</i> {duration} мин.\n<i>Описание:</i>\n{description}\n" \
+              f"<i>Рейтинг кинопоиска</i> - {rating}\n<i>Жанры:</i> {genres}\n<i>Страна:</i> {countries}\n" \
+              f"<i>Режиссер:</i> {director}\n<i>В главных ролях:</i> {actors}"
+        keyboard = InlineKeyboardMarkup()
+        kb_link_button = InlineKeyboardButton("Kinobar", url=link_kb)
+        kp_link_button = InlineKeyboardButton("Kinopoisk", url=link_kp)
+        keyboard.add(kp_link_button, kb_link_button)
+        await message.answer(msg, reply_markup=keyboard, parse_mode='HTML')
     else:
         await message.answer(f'Сожалеем, фильма с id {movie_id} не существует :(')
